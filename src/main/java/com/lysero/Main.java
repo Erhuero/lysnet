058d290897be6dcd96f114d7a350d7ae69c6fb3d
Constantin <constantin.chtanko@gmail.com>
2024-11-11 04:04:45 +0100

    Bean test

--- src/main/java/com/lysero/Main.java
+++ src/main/java/com/lysero/Main.java
@@ -3,19 +3,28 @@ package com.lysero;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args){
-        ConfigurableApplicationContext applicationContext =  SpringApplication.run(Main.class, args);
 
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public Foo getFoo(){
+        return new Foo("bar");
+    }
+    record Foo(String name){}
+
+    private static void printBeans(ConfigurableApplicationContext ctx) {
         String[] beanDefinitionNames =
-                applicationContext.getBeanDefinitionNames();
+                ctx.getBeanDefinitionNames();
 
         for (String beanDefinitionName : beanDefinitionNames) {
             System.out.println(beanDefinitionName);
         }
     }
-
 }
