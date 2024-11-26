825fee618e42b231c473e41372168452771007c0
Constantin <constantin.chtanko@gmail.com>
2024-11-26 23:50:22 +0100

    Table configuration

--- src/main/java/com/lysero/customer/Customer.java
+++ src/main/java/com/lysero/customer/Customer.java
@@ -5,21 +5,32 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Table(
+        name = "customer",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "cudtomer_email_unique",
+                        columnNames = "email"
+                )
+        }
+)
 public class Customer {
 
     @Id
     @SequenceGenerator(
-            name= "customer_id_sequence",
+            name= "customer_id_seq",
             sequenceName = "customer_id_sequence"
     )
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
-            generator = "customer_id_sequence"
+            generator = "customer_id_seq"
     )
     private Integer id;
     @Column(nullable = false)
     private String name;
-    @Column(nullable = false)
+    @Column(
+            nullable = false
+    )
     private String email;
     @Column(nullable = false)
     private Integer age;
