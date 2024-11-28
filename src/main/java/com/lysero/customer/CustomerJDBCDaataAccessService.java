5c1b512f899bbe58ef435dddff438e3eb2684c1a
Constantin <constantin.chtanko@gmail.com>
2024-11-28 21:47:25 +0100

    JDBCDataAccess

---
+++ src/main/java/com/lysero/customer/CustomerJDBCDaataAccessService.java
@@ -0,0 +1,42 @@
+package com.lysero.customer;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CustomerJDBCDaataAccessService implements CustomerDao {
+
+    @Override
+    public List<Customer> selectAllCustomers() {
+        return null;
+    }
+
+    @Override
+    public Optional<Customer> selectCustomerById(Integer id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void insertCustomer(Customer customer) {
+
+    }
+
+    @Override
+    public boolean existsPersonWithEmail(String email) {
+        return false;
+    }
+
+    @Override
+    public boolean existsPersonWithId(Integer customerId) {
+        return false;
+    }
+
+    @Override
+    public void deleteCustomerById(Integer customerId) {
+
+    }
+
+    @Override
+    public void updateCustomer(Customer update) {
+
+    }
+}
