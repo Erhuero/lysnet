3b98639f506c16e19c362860797c3eb7f0a597f2
Constantin <constantin.chtanko@gmail.com>
2024-12-02 23:59:09 +0100

    CustomerService

--- src/main/java/com/lysero/customer/CustomerService.java
+++ src/main/java/com/lysero/customer/CustomerService.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Service
 public class CustomerService {
-    //TODO
+
     private final CustomerDao customerDao;
     private final CustomerRepository customerRepository;
 
