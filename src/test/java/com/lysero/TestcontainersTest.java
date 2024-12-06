1fca3ae0f6ee131985e12c103b6afef2fe5b4d9f
Constantin <constantin.chtanko@gmail.com>
2024-12-06 23:55:42 +0100

    Test with container database

--- src/test/java/com/lysero/TestcontainersTest.java
+++ src/test/java/com/lysero/TestcontainersTest.java
@@ -1,10 +1,20 @@
 package com.lysero;
 
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 public class TestcontainersTest {
 
+    private static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest")
+            .withDatabaseName("lysero-dao-unit-test")
+            .withUsername("lysero")
+            .withPassword("password");
+
     @Test
-    void name() {
+    void canStartPostgresDB() {
+        //Given
+        //When
+
+        //Then
     }
 }
